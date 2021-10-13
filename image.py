2105ec1ef26381ab0835cfa5645e1eef4674d061
pyzak117 <86309102+pyzak117@users.noreply.github.com>
2021-10-13 17:59:37 +0200

    Update image.py

--- image.py
+++ image.py
@@ -11,8 +11,9 @@ from PIL import Image as pillow_img
 
 class Image:
     """
-    Cette classe représente une image satellite multispectrale. 
-    Pour le moment, seul les produits Landsat-8 sont pris en charge.
+    Cette classe représente une image Landsat-8 multispectrale. 
+    Ses attributs sont des matrices ndarray représentant chaque bande
+    et sont nommés selon la couleur de la bande qu'ils contiennent.
 
     attributs
     ---------
@@ -23,14 +24,42 @@ class Image:
     """
 
     def __init__(self, imgPath, zone, sensor = "LS8"):
+        """
+        description
+        -----------
+            Méthode appelée automatiquement lors de l'instanciation d'un objet Image.
+            Elle permet d'initialiser tous les attributs d'instance.
+        
+        parameters
+        ----------
+            self(Image): désigne l'instance de la classe Image sur laquelle cette fonction va agir. (N'a pas à être listé les paramètres lors de l'appel)
 
+            imgPath(str): chemin vers un dossier contenant toutes les bandes d'une image LS8
+            
+            zone(list): Contient les coordonnées du point haut-gauche et bas-droite d'une zone d'intérêt à charger.
+
+            sensor(str): Permettrait de modifier le comportement de cette fonction pour adapter
+            cette classe à d'autres types de produits, issus de capteurs différents.
+        
+        returns
+        -------
+            Cette fonction ne renvoie rien mais permet de charger des données dans les attributs
+            d'instance de type Image.
+
+        """
+
+        # Création d'un dictionnaire dans lequel on va stocker les différentes bandes
         bands = {}
+
+        # Unpack de la liste zone transmise en paramètres
         xMin, xMax, yMin, yMax = zone
 
+        # Vérification de la validité logique des coordonnées
         if xMin >= xMax or yMin >= yMax :
             raise ValueError("Coordonnées de la zone d'intérêt invalides")
 
-        print("Recherche et chargement des canaux")
+        # Information de l'utilisateur du chargement
+        print("Recherche et chargement des bandes spectrales")
 
         # Parcours de tous les fichiers dans le dossier de l'image
         for fichier in os.listdir(imgPath):
@@ -39,8 +68,12 @@ class Image:
             if fichier.upper().endswith(".TIF"):
 
                 try:
-                    # Recherche du numéro de la bande dans le nom de l'image
-                    motif = re.compile("B[0-9]+.TIF$")
+                    # Elaboration d'un motif de chaîne par expression régulière
+
+                    motif = re.compile("B[0-9]+.TIF$") # [0-9]+ = un ou plusieurs chiffres
+                                                       # $ = fin de la chaîne
+
+                    # Recherche de la position de début de ce motif dans le nom de l'image
                     pos = re.search(motif, fichier.upper()).span()[0]
 
                     # Construction de l'identifiant de la bande
@@ -75,9 +108,14 @@ class Image:
 
                     print(">>> Canal {} chargé".format(bandId))
 
+                # AttributeError peut être soulevée lors la transformation du ds en geotransform
+                # dans le cas où le nom de l'image ne comporte pas de chaîne correspondant au motif recherché (Ca veut dire que c'est pas une bande)
+                
                 except AttributeError:
+                    # On informe que le fichier n'a pas été chargé
                     print(">>> Fichier {} ignoré".format(fichier))
 
+        # Transfère les éléments du dictionnaire bands dans des attributs d'instance
         if sensor == "LS8":
             self.blue = bands["B2"]
             self.green = bands["B3"]
@@ -86,27 +124,43 @@ class Image:
             self.swir1 = bands["B6"]
             self.swir2 = bands["B7"]
 
+        return None
+
     def compute_ci1(self):
         """
-        Calcule l'indice nuageux ci1
+        Calcule l'indice nuageux ci1 à partir des bandes de l'image
         """
         ci1 = (self.blue + self.green + self.red)/(self.nir + self.swir1*2)
         return ci1
 
     def compute_ci2(self):
         """
-        Calcule l'indice nuageux ci2
+        Calcule l'indice nuageux ci2 à partir des bandes de l'image
         """
         ci2 = (self.blue + self.green + self.red + self.nir + self.swir1 + self.swir2) / 6
         return ci2
 
     def classifIndice(self, matriceIndice, mode, T1 = 0.1, t2 = 0.1):
+        """
+        description
+        -----------
+            Produit une classification binaire à partir d'une matrice et d'un seuil
+
+        parameters
+        ----------
+            matriceIndice(ndarray) : matrice à classifier
+            mode(int) : Modifie le comportement de la fonction selon la classif à réaliser
         
-        # Si on souhaite produire une classification basée sur une matrice de résultats de l'indice T1
+        returns
+        -------
+            classif(ndarray) : matrice binaire ayant des valeurs 0 et 255.
+            Les valeurs 0 correspondent aux pixels qui ne respectent pas la condition.
+            Les valeurs 255 à ceux qui la respectent.
+            0 et 255 permet d'avoir des images en noir et blanc avec pillow.show()
+        """
         if mode == 1:
             classif = np.where(matriceIndice < T1, 255, 0)
 
-        # Si on souhaite produire une classification basée sur une matrice de résultats de l'indice T2
         elif mode == 2:
 
             # Définition du seuil T2 en fonction du paramètre t2
@@ -124,13 +178,33 @@ class Image:
 
         return classif
 
-    def fusionClassifs(self, visual1, visual2):
-        return visual1 + visual2
+    def fusionClassifs(self, matrice1, matrice2):
+        """
+        Additionne deux matrices.
+        """
+        return matrice1 + matrice2
 
-    def surface_nuage(self, matrice):
-        return (np.count_nonzero(matrice)/(np.shape(matrice)[0]*np.shape(matrice)[1]))*100
+    def surfaceNonZero(self, matrice):
+        """
+        Calcule le pourcentage de valeurs différentes de 0 sur une matrice.
+        """
+
+        surfaceMat = np.shape(matrice)[0]*np.shape(matrice)[1]
+        nbPixelsBlancs = np.count_nonzero(matrice)
+        return (nbPixelsBlancs/surfaceMat)*100
 
     def matrice2png(self, matrice, mode = 0, path = ""):
+        """
+        description
+        -----------
+            Convertit une matrice ndarray en image
+        
+        parameters
+        ----------
+            matrice(ndarray) : matrice à convertir
+            mode(int) : permet d'adapter le comportement pour enregistrer une image ou simplement l'afficher
+            path(str) : nom de fichier lors de la sauvegarde de l'image
+        """
         if mode == 0:
             pillow_img.fromarray(matrice).convert("L").show()
         
@@ -142,3 +216,8 @@ class Image:
 
             except ValueError:
                 print("Chemin d'enregistrement de l'image inconnu")
+        
+        else:
+            print("Mode inconnu")
+        
+        return None
